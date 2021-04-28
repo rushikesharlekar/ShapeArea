995cb9635fc09ba658ccc60de54d075e6dbaeedc
Rushikesh Arlekar <70571856+rushikesharlekar@users.noreply.github.com>
2021-04-28 15:41:28 +0530

    Update Circle.java

--- src/Area/Circle.java
+++ src/Area/Circle.java
@@ -12,13 +12,13 @@ public class Circle implements Shape {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public float calculateArea() {
 		float area = (float) (2 * 3.14 * this.radius);
 		return area;
 	}
 
-	@Override
+	
 	public void setAttribute(int i, int j) {
 		this.radius = i;
 	}
