c30948ed3dbaf70f90b5d9129741e424ae87abdf
Rushikesh Arlekar <70571856+rushikesharlekar@users.noreply.github.com>
2021-04-28 15:44:15 +0530

    Update Triangle.java

--- src/Area/Triangle.java
+++ src/Area/Triangle.java
@@ -17,13 +17,13 @@ public class Triangle implements Shape{
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public float calculateArea() {
 		float area = (float) (0.5 * this.base * this.height);
 		return area;
 	}
 
-	@Override
+	
 	public void setAttribute(int i, int j) {
 		this.base = i;
 		this.height = j;
